413c878fbd517355f79147f4b6b1fae89a2f72ff
mostafa-abokhadra <mvxizk@gmail.com>
2024-07-13 16:38:43 +0300

    adding #4

---
+++ testing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+    
+}
+
+
+
+what is the output:
+correct: not similar
+wrong: both strings are similar
+wrong: hello world
+wrong: syntax error in strcmp
